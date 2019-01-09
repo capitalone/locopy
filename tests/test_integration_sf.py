97b0a18db95454d0fb176eb1d99d1d75fbbb97be
Faisal Dosani <faisal.dosani@capitalone.com>
2019-01-09 13:58:47 -0500

    adding integration tests and docs

---
+++ tests/test_integration_sf.py
@@ -0,0 +1,129 @@
+# MIT License
+#
+# Copyright (c) 2018 Capital One Services, LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import filecmp
+import pytest
+import snowflake.connector
+import pandas as pd
+import numpy as np
+import boto3
+import locopy
+
+from pathlib import Path
+
+
+DBAPIS = [snowflake.connector]
+INTEGRATION_CREDS = str(Path.home()) + "/.locpy-sfrc"
+S3_BUCKET = "locopy-integration-testing"
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+LOCAL_FILE = os.path.join(CURR_DIR, "data", "mock_file.txt")
+LOCAL_FILE_DL = os.path.join(CURR_DIR, "data", "mock_file_dl.txt")
+
+CREDS_DICT = locopy.utility.read_config_yaml(INTEGRATION_CREDS)
+
+
+@pytest.fixture()
+def s3_bucket():
+    session = boto3.Session(profile_name=CREDS_DICT["profile"])
+    c = session.client("s3")
+    c.create_bucket(Bucket=S3_BUCKET)
+    yield c
+    r = session.resource("s3").Bucket(S3_BUCKET)
+    r.objects.all().delete()
+    r.delete()
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize("dbapi", DBAPIS)
+def test_snowflake_execute_single_rows(dbapi):
+
+    expected = pd.DataFrame({"field_1": [1], "field_2": [2]})
+    with locopy.Snowflake(dbapi=dbapi, **CREDS_DICT) as test:
+        test.execute("SELECT 1 AS field_1, 2 AS field_2 ")
+        df = test.to_dataframe()
+
+    assert np.allclose(df["field_1"], expected["field_1"])
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize("dbapi", DBAPIS)
+def test_snowflake_execute_multiple_rows(dbapi):
+
+    expected = pd.DataFrame({"field_1": [1, 2], "field_2": [1, 2]})
+    with locopy.Snowflake(dbapi=dbapi, **CREDS_DICT) as test:
+        test.execute(
+            "SELECT 1 AS field_1, 1 AS field_2 " "UNION " "SELECT 2 AS field_1, 2 AS field_2"
+        )
+        df = test.to_dataframe()
+
+    assert np.allclose(df["field_1"], expected["field_1"])
+    assert np.allclose(df["field_2"], expected["field_2"])
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize("dbapi", DBAPIS)
+def test_upload_to_internal(dbapi):
+
+    with locopy.Snowflake(dbapi=dbapi, **CREDS_DICT) as test:
+        # delete if exists
+        test.execute("REMOVE @~/staged/mock_file.txt.gz")
+
+        # test
+        test.upload_to_internal(LOCAL_FILE, "@~/staged/")
+        test.execute("LIST @~/staged/mock_file.txt")
+        res = test.cursor.fetchall()
+        assert res[0][0] == "staged/mock_file.txt.gz"
+
+        # clean up
+        test.execute("REMOVE @~/staged/mock_file.txt.gz")
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize("dbapi", DBAPIS)
+def test_copy(dbapi):
+
+    with locopy.Snowflake(dbapi=dbapi, **CREDS_DICT) as test:
+        test.upload_to_internal(LOCAL_FILE, "@~/staged/")
+        test.execute("USE SCHEMA {}".format(CREDS_DICT["schema"]))
+        test.execute(
+            "CREATE TEMPORARY TABLE locopy_integration_testing (id INTEGER, variable VARCHAR(20))"
+        )
+        test.copy(
+            "locopy_integration_testing",
+            "@~/staged/mock_file.txt.gz",
+            delim="|",
+            copy_options=["PURGE = TRUE"],
+        )
+        test.execute("SELECT * FROM locopy_integration_testing ORDER BY id")
+        results = test.cursor.fetchall()
+
+        expected = [
+            (1, "This iš line 1"),
+            (2, "This is liné 2"),
+            (3, "This is line 3"),
+            (4, "This is lïne 4"),
+        ]
+
+        for i, result in enumerate(results):
+            assert result[0] == expected[i][0]
+            assert result[1] == expected[i][1]
